2fe9376d00d5dd89aa7fac118f0dc0025943fecd
Peter Simone <plssimone@gmail.com>
2023-05-23 19:10:14 -0400

    begin openai + isolated indeed page search

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import puppeteer from "puppeteer-core";
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+import * as dotenv from "dotenv";
+import fetch from "node-fetch";
+dotenv.config();
 
 //Scrape LinkedIn, indeed, and builtinnyc
 
@@ -22,41 +23,74 @@ dotenv.config()
 //   parser(el).then(console.log).catch(console.error);
 // }
 
-async function scrape(url, browser, selector, parser) {
-  const page = await browser.newPage();
+const openai = async (data) => {
 
-  await page.setDefaultNavigationTimeout(2 * 60 * 1000);
+  const prompt = `Here is a job description:\n\n${data}\n\nIs this description based off a job posting from a design agency? (YES/NO)`;
 
-  await page.goto(url);
+  const body = {
+    "model": "text-davinci-003",
+    "prompt": prompt,
+    "max_tokens": 3,
+  };
 
-  // Use the specific Indeed selector here, to make sure the page has loaded the relevant content
-  await page.waitForSelector(selector);
+  const headers = {
+    "Authorization": `Bearer ${process.env.OPENAI_KEY}`,
+    "Content-Type": "application/json",
+  };
 
-  console.log("HTML FROM " + url + ": ");
+  const response = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers,
+    body: JSON.stringify(body),
+  });
 
-  try {
-    const jobs = await parser(page);
-    console.log(jobs);
-  } catch (error) {
-    console.error(error);
-  }
+  const result = await response.json();
+
+  const text = result.choices[0].text
+  
+  return text.replace(/^\s+|\s+$/g, '').toUpperCase();
 }
 
-async function getIndeedJobListings(page) {
+const getSingleIndeedListingSummary = async (page, job) => {
+
+  if (!job.url) {
+    return "NAN";
+  }
+
+  try {
+    await page.goto(job.url);
+    await page.waitForSelector("#jobDescriptionText");
+  } catch(error) {
+    return "NAN";
+  }
+
+  const description = await page.evaluate(() => {
+    return document.getElementById("jobDescriptionText")?.innerText;
+  })
+
+  const isAgency = await openai(description);
+
+  job.isAgency = isAgency;
+
+  return job;
+}
+
+const getIndeedJobListings = async (page) => {
 
   const jobs = await page.evaluate(() => {
     const jobNodes = document.querySelectorAll('.jobsearch-ResultsList > li');
-    const jobListings = Array.from(jobNodes).map(jobNode => {
-      const title = jobNode.querySelector('.jobTitle > a > span')?.innerText;
-      const companyName = jobNode.querySelector('.companyName')?.innerText;
-      const location = jobNode.querySelector('.companyLocation')?.innerText;
-      const salary = jobNode.querySelector('.salary-snippet-container')?.innerText;
-      const jobType = jobNode.querySelector('.metadataContainer > .metadata > div > svg[aria-label="Job type"]')?.nextSibling?.nodeValue;
-      const shift = jobNode.querySelector('.metadataContainer > .metadata > div > svg[aria-label="Shift"]')?.nextSibling?.nodeValue;
-      const experienceRequired = jobNode.querySelector('.metadataContainer > .attribute_snippet')?.innerText;
-      const description = jobNode.querySelector('.job-snippet')?.innerText;
-      const daysPosted = jobNode.querySelector('.result-footer > .date')?.innerText;
-      return { title, companyName, location, salary, jobType, shift, experienceRequired, description, daysPosted };
+    const jobListings = Array.from(jobNodes).slice(0,5).map(jobNode => {
+      const title = jobNode.querySelector('.jobTitle > a > span')?.innerText | "";
+      const companyName = jobNode.querySelector('.companyName')?.innerText | "";
+      const location = jobNode.querySelector('.companyLocation')?.innerText | "";
+      const salary = jobNode.querySelector('.salary-snippet-container')?.innerText | "";
+      const jobType = jobNode.querySelector('.metadataContainer > .metadata > div > svg[aria-label="Job type"]')?.nextSibling?.nodeValue | "";
+      const shift = jobNode.querySelector('.metadataContainer > .metadata > div > svg[aria-label="Shift"]')?.nextSibling?.nodeValue | "";
+      const experienceRequired = jobNode.querySelector('.metadataContainer > .attribute_snippet')?.innerText | "";
+      const description = jobNode.querySelector('.job-snippet')?.innerText | "";
+      const daysPosted = jobNode.querySelector('.result-footer > .date')?.innerText | "";
+      const url = jobNode.querySelector("a")?.getAttribute("href") | "";
+      return { title, companyName, location, salary, jobType, shift, experienceRequired, description, daysPosted, url };
     });
     return jobListings;
   });
@@ -64,11 +98,11 @@ async function getIndeedJobListings(page) {
   return jobs;
 }
 
-async function parseLinkedIn() {
+const parseLinkedIn = async () => {
 
 }
 
-async function getBuiltInNYCJobListings(page) {
+const getBuiltInNYCJobListings = async (page) => {
 
   const jobs = await page.evaluate(() => {
     const jobNodes = document.querySelectorAll('.v-lazy.job-item');
@@ -88,14 +122,45 @@ async function getBuiltInNYCJobListings(page) {
   return jobs;
 }
 
-async function run() {
+const scrape = async (url, browser, selector, parser) => {
+  const page = await browser.newPage();
+  // await page.setViewport({
+  //   width: 1512,
+  //   height: 945,
+  // });
+
+  await page.setDefaultNavigationTimeout(2 * 60 * 1000);
+
+  await page.goto(url);
+
+  // Use the specific Indeed selector here, to make sure the page has loaded the relevant content
+  await page.waitForSelector(selector);
+
+  console.log("HTML FROM " + url + ": ");
+
+  let jobs;
+  try {
+    jobs = await parser(page);
+  } catch (error) {
+    console.error(error);
+    return Promise.reject("couldn't parse jobs.");
+  }
+
+  console.log(jobs);
+
+  jobs = await Promise.allSettled(jobs.map((job) => {
+    return getSingleIndeedListingSummary(page, job);
+  }))
+
+  return jobs;
+}
+
+const run = async () => {
   let browser;
 
   try {
 
     const auth = process.env.USERNAME + ':' + process.env.PASSWORD;
-
-    console.log(auth);
 
     browser = await puppeteer.connect({
       browserWSEndpoint: `wss://${auth}@zproxy.lum-superproxy.io:9222`
@@ -104,10 +169,12 @@ async function run() {
     const indeedSelector = ".jobsearch-ResultsList > li";
     const builtInNYCSelector = ".v-lazy.job-item";
 
-    await scrape('https://www.indeed.com/jobs?q=designer&l=new+york%2C+ny&vjk=50721f52d7302fd5',
+    const jobs = await scrape('https://www.indeed.com/jobs?q=designer&l=new+york%2C+ny&radius=35&fromage=7',
         browser, indeedSelector, getIndeedJobListings);
 
-    await scrape('https://www.builtinnyc.com/jobs/design-ux', browser, builtInNYCSelector, getBuiltInNYCJobListings);
+    console.log(jobs);
+
+    // await scrape('https://www.builtinnyc.com/jobs/design-ux', browser, builtInNYCSelector, getBuiltInNYCJobListings);
     //TODO: Find URL that works for linkedin scraper
     //    await scrape('linkedin.com', browser);
 
@@ -123,3 +190,4 @@ async function run() {
 }
 
 run();
+// openai()
